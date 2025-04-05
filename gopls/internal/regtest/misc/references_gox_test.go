077305dd6c4ee66b3f652199d726aabbda2a2ded
luoliwoshang <2643523683@qq.com>
2025-04-05 10:39:09 +0800

    test:migrate ref test

--- gopls/internal/regtest/misc/references_gox_test.go
+++ gopls/internal/regtest/misc/references_gox_test.go
@@ -9,7 +9,7 @@ import (
 	. "golang.org/x/tools/gopls/internal/lsp/regtest"
 )
 
-func TestReferencesOnOverloadDecl1(t *testing.T) {
+func TestRefOverloadDeclAnony(t *testing.T) {
 	const files = `
 -- go.mod --
 module mod.com
@@ -44,28 +44,19 @@ func main() {
 	fmt.Println(add__1("Hello", "World"))
 }
 `
-	Run(t, files, func(t *testing.T, env *Env) {
-		env.OpenFile("def.gop")
-		loc := env.GoToDefinition(env.RegexpSearch("def.gop", `add`))
-		refs, err := env.Editor.References(env.Ctx, loc)
-		if err != nil {
-			t.Fatalf("references on (*s).Error failed: %v", err)
-		}
-		var buf strings.Builder
-		for _, ref := range refs {
-			fmt.Fprintf(&buf, "%s %s\n", env.Sandbox.Workdir.URIToPath(ref.URI), ref.Range)
-		}
-		got := buf.String()
-		want := "def.gop 0:5-0:8\n" + // overload decl
-			"test.gop 0:8-0:11\n" + // overload int call
-			"test.gop 1:8-1:11\n" // overload string call
-		if diff := cmp.Diff(want, got); diff != "" {
-			t.Errorf("unexpected references on (*s).Error (-want +got):\n%s", diff)
-		}
-	})
+	testCases := []refTest{
+		{
+			"def.gop", "add", []string{
+				"def.gop 0:5-0:8",   // overload decl
+				"test.gop 0:8-0:11", // overload int call
+				"test.gop 1:8-1:11", // overload string call
+			},
+		},
+	}
+	runFindRefTest(t, files, testCases)
 }
 
-func TestReferencesOnOverloadDecl2(t *testing.T) {
+func TestRefOverloadDeclNamedAndAnony(t *testing.T) {
 	const files = `
 -- go.mod --
 module mod.com
@@ -113,50 +104,32 @@ func main() {
 	fmt.Println(mulFloat(1.2, 3.14))
 }
 `
-	// goxls: overload decl reference
-	Run(t, files, func(t *testing.T, env *Env) {
-		env.OpenFile("def.gop")
-		loc := env.GoToDefinition(env.RegexpSearch("def.gop", `func (mul) = \(`))
-		refs, err := env.Editor.References(env.Ctx, loc)
-		if err != nil {
-			t.Fatalf("references on (*s).Error failed: %v", err)
-		}
-		var buf strings.Builder
-		for _, ref := range refs {
-			fmt.Fprintf(&buf, "%s %s\n", env.Sandbox.Workdir.URIToPath(ref.URI), ref.Range)
-		}
-		got := buf.String()
-		want := "def.gop 8:5-8:8\n" + // overload defintion
-			"test.gop 0:8-0:11\n" + // overload int call
-			"test.gop 1:8-1:11\n" + // overload string call
-			"test.gop 2:8-2:11\n" // overload float call
-		if diff := cmp.Diff(want, got); diff != "" {
-			t.Errorf("unexpected references on (*s).Error (-want +got):\n%s", diff)
-		}
-	})
-	// goxls: overload member reference
-	Run(t, files, func(t *testing.T, env *Env) {
-		env.OpenFile("def.gop")
-		loc := env.GoToDefinition(env.RegexpSearch("def.gop", `func mul = \(\n\s+(mulInt)`))
-		refs, err := env.Editor.References(env.Ctx, loc)
-		if err != nil {
-			t.Fatalf("references on (*s).Error failed: %v", err)
-		}
-		var buf strings.Builder
-		for _, ref := range refs {
-			fmt.Fprintf(&buf, "%s %s\n", env.Sandbox.Workdir.URIToPath(ref.URI), ref.Range)
-		}
-		got := buf.String()
-		want := "def.gop 0:5-0:11\n" + // mulInt
-			"def.gop 9:4-9:10\n" + // overload mulInt
-			"test.gop 0:8-0:11\n" // use overload mulInt
-		if diff := cmp.Diff(want, got); diff != "" {
-			t.Errorf("unexpected references on (*s).Error (-want +got):\n%s", diff)
-		}
-	})
+
+	testCases := []refTest{
+		// goxls: overload reference
+		{
+			"def.gop", `func (mul) = \(`,
+			[]string{
+				"def.gop 8:5-8:8",   // overload defintion
+				"test.gop 0:8-0:11", // overload int call
+				"test.gop 1:8-1:11", // overload string call
+				"test.gop 2:8-2:11", // overload float call
+			},
+		},
+		// goxls: overload member reference
+		{
+			"def.gop", `func mul = \(\n\s+(mulInt)`,
+			[]string{
+				"def.gop 0:5-0:11",  // mulInt
+				"def.gop 9:4-9:10",  // overload mulInt
+				"test.gop 0:8-0:11", // use overload mulInt
+			},
+		},
+	}
+	runFindRefTest(t, files, testCases)
 }
 
-func TestReferencesOnOverloadDecl3(t *testing.T) {
+func TestRefOverloadDeclMethod(t *testing.T) {
 	const files = `
 -- go.mod --
 module mod.com
@@ -202,42 +175,51 @@ var c = a.mulFoo(a)
 func main() {
 }
 `
+	testCases := []refTest{
+		{
+			"def.gop", `func \(foo\)\.(mul) = \(`,
+			[]string{
+				"def.gop 8:11-8:14",
+				"test.gop 1:10-1:13",
+				"test.gop 2:10-2:13",
+			},
+		},
+		{
+			"def.gop", `\(foo\)\.(mulInt)`,
+			[]string{
+				"def.gop 2:14-2:20",
+				"def.gop 9:10-9:16",
+				"test.gop 1:10-1:13",
+			},
+		},
+	}
+	runFindRefTest(t, files, testCases)
+}
+
+type refTest struct {
+	defineFile   string
+	defineLocReg string
+	refLocs      []string
+}
+
+func runFindRefTest(t *testing.T, files string, testCase []refTest) {
 	Run(t, files, func(t *testing.T, env *Env) {
-		env.OpenFile("def.gop")
-		loc := env.GoToDefinition(env.RegexpSearch("def.gop", `func \(foo\)\.(mul) = \(`))
-		refs, err := env.Editor.References(env.Ctx, loc)
-		if err != nil {
-			t.Fatalf("references on (*s).Error failed: %v", err)
-		}
-		var buf strings.Builder
-		for _, ref := range refs {
-			fmt.Fprintf(&buf, "%s %s\n", env.Sandbox.Workdir.URIToPath(ref.URI), ref.Range)
-		}
-		got := buf.String()
-		want := "def.gop 8:11-8:14\n" +
-			"test.gop 1:10-1:13\n" +
-			"test.gop 2:10-2:13\n"
-		if diff := cmp.Diff(want, got); diff != "" {
-			t.Errorf("unexpected references on (*s).Error (-want +got):\n%s", diff)
-		}
-	})
-	Run(t, files, func(t *testing.T, env *Env) {
-		env.OpenFile("def.gop")
-		loc := env.GoToDefinition(env.RegexpSearch("def.gop", `\(foo\)\.(mulInt)`))
-		refs, err := env.Editor.References(env.Ctx, loc)
-		if err != nil {
-			t.Fatalf("references on (*s).Error failed: %v", err)
-		}
-		var buf strings.Builder
-		for _, ref := range refs {
-			fmt.Fprintf(&buf, "%s %s\n", env.Sandbox.Workdir.URIToPath(ref.URI), ref.Range)
-		}
-		got := buf.String()
-		want := "def.gop 2:14-2:20\n" +
-			"def.gop 9:10-9:16\n" +
-			"test.gop 1:10-1:13\n"
-		if diff := cmp.Diff(want, got); diff != "" {
-			t.Errorf("unexpected references on (*s).Error (-want +got):\n%s", diff)
+		for _, test := range testCase {
+			env.OpenFile(test.defineFile)
+			loc := env.GoToDefinition(env.RegexpSearch(test.defineFile, test.defineLocReg))
+			refs, err := env.Editor.References(env.Ctx, loc)
+			if err != nil {
+				t.Fatalf("references on (*s).Error failed: %v", err)
+			}
+			var buf strings.Builder
+			for _, ref := range refs {
+				fmt.Fprintf(&buf, "%s %s\n", env.Sandbox.Workdir.URIToPath(ref.URI), ref.Range)
+			}
+			got := buf.String()
+			want := strings.Join(test.refLocs, "\n") + "\n"
+			if diff := cmp.Diff(want, got); diff != "" {
+				t.Errorf("unexpected references on (*s).Error (-want +got):\n%s", diff)
+			}
 		}
 	})
 }
